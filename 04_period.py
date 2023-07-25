99ea2b2f8206b521c80a11e973ace5d11c84043d
chyijiunn <chyijiunn@users.noreply.github.com>
2023-07-25 15:44:46 +0800

    ud

--- 04_period.py
+++ 04_period.py
@@ -3,10 +3,10 @@ from utime import sleep
 servo = PWM(Pin(0))
 servo.freq(50)
 
-def buzz(tim):
+def motor(tim):
     servo.duty_u16(5000)
     sleep(0.1)
     servo.duty_u16(3000)
 
 tim = Timer(-1)
-tim.init(period=1000, mode=Timer.PERIODIC, callback=buzz)
+tim.init(period=1000, mode=Timer.PERIODIC, callback=motor)
